c86bdbddb7fa98ba101e8c5ce787fa71d15d01fb
shacker <hacker65536@users.noreply.github.com>
2014-10-31 10:49:59 +0900

    Update hostname.sh

--- aws/hostname.sh
+++ aws/hostname.sh
@@ -16,7 +16,7 @@ if [ -e $hostfile -a -s $hostfile ];then
 
 else
 
-  id=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | /usr/local/bin/jq '.instanceId' | tr -d '"')
+  id=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|/usr/local/bin/jq '.instanceId'|tr -d '"')
   region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | /usr/local/bin/jq '.region' | tr -d '"')
 
   /bin/ping example.com -w 3 >/dev/null 2>&1
@@ -35,6 +35,8 @@ else
     reg='syd';;
     eu-west-1 )
     reg='irl';;
+    eu-central-1)
+    reg='fra';;
     sa-esat-1 )
     reg='sao';;
     us-east-1 )
