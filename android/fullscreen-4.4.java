4b6d059353cdcef7ab3ac0b3d5afe8389f2bf6ec
shacker <hacker65536@users.noreply.github.com>
2015-09-29 16:16:04 +0900

    Update fullscreen-4.4.java

--- android/fullscreen-4.4.java
+++ android/fullscreen-4.4.java
@@ -1,7 +1,11 @@
+//https://developer.android.com/training/system-ui/immersive.html
+
 @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
         View decor = this.getWindow().getDecorView();
-        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  
+        | View.SYSTEM_UI_FLAG_FULLSCREEN 
+        | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
