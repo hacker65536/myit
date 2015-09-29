daac09df36a6e18489760c2d36c5ad3224fbed9a
shacker <hacker65536@users.noreply.github.com>
2015-09-29 16:15:28 +0900

    Create fullscreen-4.4.java

---
+++ android/fullscreen-4.4.java
@@ -0,0 +1,7 @@
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        View decor = this.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
