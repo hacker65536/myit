3ec1ec1cb0d366cd9a8b82955cc86012cbddf890
hacker65536 <s.hacker65536@gmail.com>
2019-07-01 11:16:14 +0900

    Update tfupdate.sh

--- terraform/tfupdate.sh
+++ terraform/tfupdate.sh
@@ -4,11 +4,12 @@ function get_latest_tfver() {
   terraform -v | sed -n 's/^is \(\([0-9]\{1,3\}\.\)\{2\}[0-9]\{1,3\}\)\(.*\)/\1/p'
 }
 
-latestver=$(get_latest_tfver)
+function updatetf(){
+  latestver=$(get_latest_tfver)
+  curl -SsL -O https://releases.hashicorp.com/terraform/${latestver}/terraform_${latestver}_linux_amd64.zip
+  sudo unzip terraform_${latestver}_linux_amd64.zip
+  sudo mv terraform /usr/local/bin/terraform
+  rm terraform_${latestver}_linux_amd64.zip
+}
 
-echo $latestver
-
-curl -SsL -O https://releases.hashicorp.com/terraform/${latestver}/terraform_${latestver}_linux_amd64.zip
-sudo unzip terraform_${latestver}_linux_amd64.zip
-sudo mv terraform /usr/local/bin/terraform
-rm terraform_${latestver}_linux_amd64.zip
+terraform -v | grep 'out of date' && updatetf || echo "version of terraform is latest"
